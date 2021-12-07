8f4e5a86447a395673960e23e7c8e38b91707cf6
NachiLuong <lqtrung20122001@gmail.com>
2021-12-08 01:07:42 +0700

    trung dep trai

--- src/main/java/com/ezcook/controllers/web/LoadMoreController.java
+++ src/main/java/com/ezcook/controllers/web/LoadMoreController.java
@@ -22,121 +22,121 @@ public class LoadMoreController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (SessionUtil.getInstance().getValue(req, "user")!= null){
-            if (req.getServletPath().equals("/loadMoreHome")) {
-                int count = Integer.parseInt(req.getParameter("count"));
-                FoodDao foodDao = new FoodDao();
-                FoodService cs = new FoodService();
-                List<Food> listFoodNew = foodDao.getListFoodNew(count, 6);
-                //PrintWriter out=resp.getWriter();
-                ServletOutputStream out = resp.getOutputStream();
-                for (Food food : listFoodNew) {
-                  //  ("<a href=\"" + cc + "\"")
-                    String cc= "/blog?id=" + food.getId();
-                    out.write("<div class=\"food item text-center\">\n".getBytes(StandardCharsets.UTF_8));
+        /*if (SessionUtil.getInstance().getValue(req, "user")!= null){
 
-                    out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
+        }else {
+            resp.sendRedirect("/login");
+        }*/
+        if (req.getServletPath().equals("/loadMoreHome")) {
+            int count = Integer.parseInt(req.getParameter("count"));
+            FoodDao foodDao = new FoodDao();
+            FoodService cs = new FoodService();
+            List<Food> listFoodNew = foodDao.getListFoodNew(count, 6);
+            //PrintWriter out=resp.getWriter();
+            ServletOutputStream out = resp.getOutputStream();
+            for (Food food : listFoodNew) {
+                //  ("<a href=\"" + cc + "\"")
+                String cc= "/blog?id=" + food.getId();
+                out.write("<div class=\"food item text-center\">\n".getBytes(StandardCharsets.UTF_8));
+
+                out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
                    /* out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
                     out.write(">".getBytes(StandardCharsets.UTF_8));*/
 
-                    out.write("<img src=\"".getBytes(StandardCharsets.UTF_8));
-                    out.write(food.getImage().getBytes(StandardCharsets.UTF_8));
-                    out.write("\" alt=\"".getBytes(StandardCharsets.UTF_8));
-                    out.write(food.getName().getBytes(StandardCharsets.UTF_8));
-                    out.write("\"/></a>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<div>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<span>Ezook <i class=\"fal fa-utensils-alt\"></i> Đồ uống</span>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<img src=\"".getBytes(StandardCharsets.UTF_8));
+                out.write(food.getImage().getBytes(StandardCharsets.UTF_8));
+                out.write("\" alt=\"".getBytes(StandardCharsets.UTF_8));
+                out.write(food.getName().getBytes(StandardCharsets.UTF_8));
+                out.write("\"/></a>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<div>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<span>Ezook <i class=\"fal fa-utensils-alt\"></i> Đồ uống</span>\n".getBytes(StandardCharsets.UTF_8));
 
-                    out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
+                out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
                    /* out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
                     out.write(">".getBytes(StandardCharsets.UTF_8));*/
 
-                    out.write("<h5>".getBytes(StandardCharsets.UTF_8));
-                    out.write(food.getName().getBytes(StandardCharsets.UTF_8));
-                    out.write("</h5>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("</a>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<span class=\"author\">by <a href=\"<c:url value='/#'/>\">Ezcook</a></span>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<p id=\"demo\"></p>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<hr>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<p class=\"content-food\">".getBytes(StandardCharsets.UTF_8));
-                    out.write(cs.getTextBetweenTags("p", food.getContent()).getBytes(StandardCharsets.UTF_8));
-                    out.write("</p>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<h5>".getBytes(StandardCharsets.UTF_8));
+                out.write(food.getName().getBytes(StandardCharsets.UTF_8));
+                out.write("</h5>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("</a>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<span class=\"author\">by <a href=\"<c:url value='/#'/>\">Ezcook</a></span>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<p id=\"demo\"></p>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<hr>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<p class=\"content-food\">".getBytes(StandardCharsets.UTF_8));
+                out.write(cs.getTextBetweenTags("p", food.getContent()).getBytes(StandardCharsets.UTF_8));
+                out.write("</p>\n".getBytes(StandardCharsets.UTF_8));
 
-                    out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
+                out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
                     /*out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
                     out.write("\">Xem tiếp...</a>\n".getBytes(StandardCharsets.UTF_8));*/
 
-                    out.write("<div class=\"social-icon\">\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<a href=\"<c:url value='/#'/>\"><i class=\"fab fa-facebook-f\"></i></a>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<a href=\"<c:url value='/#'/>\"><i class=\"fab fa-google-plus-g\"></i></a>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<a href=\"<c:url value='/#'/>\"><i class=\"fab fa-twitter\"></i></a>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<a href=\"<c:url value='/#'/>\"><i class=\"fab fa-instagram\"></i></a>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("</div>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("</div>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("</div>".getBytes(StandardCharsets.UTF_8));
-                }
+                out.write("<div class=\"social-icon\">\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<a href=\"<c:url value='/#'/>\"><i class=\"fab fa-facebook-f\"></i></a>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<a href=\"<c:url value='/#'/>\"><i class=\"fab fa-google-plus-g\"></i></a>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<a href=\"<c:url value='/#'/>\"><i class=\"fab fa-twitter\"></i></a>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<a href=\"<c:url value='/#'/>\"><i class=\"fab fa-instagram\"></i></a>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("</div>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("</div>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("</div>".getBytes(StandardCharsets.UTF_8));
             }
-            else
-            {
-                String id = (String) req.getSession().getAttribute("id");
-                System.out.println(id);
-                long foodTypeID = Long.parseLong(id);
-                int count = Integer.parseInt(req.getParameter("count"));
-                FoodDao foodDao = new FoodDao();
-                FoodService cs = new FoodService();
-                List<Food> listFood = foodDao.getListByFoodTypeIDAndCount(foodTypeID,count,6);
-                //PrintWriter out=resp.getWriter();
-                ServletOutputStream out = resp.getOutputStream();
-                for (Food food : listFood) {
-                    out.write("<div class=\"food content\">\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<div class=\"image\">".getBytes(StandardCharsets.UTF_8));
-
-                    out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
-                    /*out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
-                    out.write(">".getBytes(StandardCharsets.UTF_8));*/
-
-                    out.write("<img src=\"".getBytes(StandardCharsets.UTF_8));
-                    out.write(food.getImage().getBytes(StandardCharsets.UTF_8));
-                    out.write("\" alt=\"".getBytes(StandardCharsets.UTF_8));
-                    out.write(food.getName().getBytes(StandardCharsets.UTF_8));
-                    out.write("\"/></a>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("</div>".getBytes(StandardCharsets.UTF_8));
-                    out.write("<div class=\"item text-center\">\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<span>Ezook <i class=\"fal fa-utensils-alt\"></i>".getBytes(StandardCharsets.UTF_8));
-                    out.write(food.getFoodtype().getName().getBytes(StandardCharsets.UTF_8));
-                    out.write("</span>\n".getBytes(StandardCharsets.UTF_8));
-
-                    out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
-                    /*out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
-                    out.write(">".getBytes(StandardCharsets.UTF_8));*/
-
-                    out.write("<h5>".getBytes(StandardCharsets.UTF_8));
-                    out.write(food.getName().getBytes(StandardCharsets.UTF_8));
-                    out.write("</h5>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("</a>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<span class=\"author\">written by <a href=\"<c:url value='/#'/>\">Ezcook | </a></span>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write(cs.formatTime(food.getCreatedOn()).getBytes(StandardCharsets.UTF_8));
-                    out.write("<p id=\"demo\"></p>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<hr>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<p class=\"food-content\">".getBytes(StandardCharsets.UTF_8));
-                    out.write(cs.getTextBetweenTags("p", food.getContent()).getBytes(StandardCharsets.UTF_8));
-                    out.write("</p>\n".getBytes(StandardCharsets.UTF_8));
-
-                    out.write("<a href=\"/blog?id=".getBytes(StandardCharsets.UTF_8));
-                    out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
-                    out.write("\">Xem tiếp...</a>\n".getBytes(StandardCharsets.UTF_8));
-
-                    out.write("</div>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("</div>\n".getBytes(StandardCharsets.UTF_8));
-                    out.write("<hr>".getBytes(StandardCharsets.UTF_8));
-                }
-            }
-        }else {
-            resp.sendRedirect("/login");
         }
+        else
+        {
+            String id = (String) req.getSession().getAttribute("id");
+            System.out.println(id);
+            long foodTypeID = Long.parseLong(id);
+            int count = Integer.parseInt(req.getParameter("count"));
+            FoodDao foodDao = new FoodDao();
+            FoodService cs = new FoodService();
+            List<Food> listFood = foodDao.getListByFoodTypeIDAndCount(foodTypeID,count,6);
+            //PrintWriter out=resp.getWriter();
+            ServletOutputStream out = resp.getOutputStream();
+            for (Food food : listFood) {
+                out.write("<div class=\"food content\">\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<div class=\"image\">".getBytes(StandardCharsets.UTF_8));
 
+                out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
+                    /*out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
+                    out.write(">".getBytes(StandardCharsets.UTF_8));*/
+
+                out.write("<img src=\"".getBytes(StandardCharsets.UTF_8));
+                out.write(food.getImage().getBytes(StandardCharsets.UTF_8));
+                out.write("\" alt=\"".getBytes(StandardCharsets.UTF_8));
+                out.write(food.getName().getBytes(StandardCharsets.UTF_8));
+                out.write("\"/></a>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("</div>".getBytes(StandardCharsets.UTF_8));
+                out.write("<div class=\"item text-center\">\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<span>Ezook <i class=\"fal fa-utensils-alt\"></i>".getBytes(StandardCharsets.UTF_8));
+                out.write(food.getFoodtype().getName().getBytes(StandardCharsets.UTF_8));
+                out.write("</span>\n".getBytes(StandardCharsets.UTF_8));
+
+                out.write(("<a href=\"/blog?id=" + food.getId() + "\">").getBytes(StandardCharsets.UTF_8));
+                    /*out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
+                    out.write(">".getBytes(StandardCharsets.UTF_8));*/
+
+                out.write("<h5>".getBytes(StandardCharsets.UTF_8));
+                out.write(food.getName().getBytes(StandardCharsets.UTF_8));
+                out.write("</h5>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("</a>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<span class=\"author\">written by <a href=\"<c:url value='/#'/>\">Ezcook | </a></span>\n".getBytes(StandardCharsets.UTF_8));
+                out.write(cs.formatTime(food.getCreatedOn()).getBytes(StandardCharsets.UTF_8));
+                out.write("<p id=\"demo\"></p>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<hr>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<p class=\"food-content\">".getBytes(StandardCharsets.UTF_8));
+                out.write(cs.getTextBetweenTags("p", food.getContent()).getBytes(StandardCharsets.UTF_8));
+                out.write("</p>\n".getBytes(StandardCharsets.UTF_8));
+
+                out.write("<a href=\"/blog?id=".getBytes(StandardCharsets.UTF_8));
+                out.write(String.valueOf(food.getId()).getBytes("UTF-8"));
+                out.write("\">Xem tiếp...</a>\n".getBytes(StandardCharsets.UTF_8));
+
+                out.write("</div>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("</div>\n".getBytes(StandardCharsets.UTF_8));
+                out.write("<hr>".getBytes(StandardCharsets.UTF_8));
+            }
+        }
     }
 
     @Override
---
+++ target/ezcook-1.0-SNAPSHOT/views/notfound/notfound.jsp
@@ -0,0 +1,33 @@
+<%@ include file="/common/taglib.jsp" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<c:url value='/templates/notfound/style.css' />"/>
+    <link rel="stylesheet" href="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif">
+
+    <title>Lỗi rồi</title>
+</head>
+<body>
+<section class="page_404">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 ">
+                <div class="col-sm-10 col-sm-offset-1  text-center">
+                    <div class="four_zero_four_bg">
+                        <h1 class="text-center ">EzCook: Lỗi rồi</h1>
+                    </div>
+                    <div class="contant_box_404">
+                        <h3 class="h2">
+                            The page you are looking for not avaible!
+                        </h3>
+                        <p>Quay lại trang chủ</p>
+                        <a href="/home" class="link_404">Go to Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
